556e40acb26ddd529b78e00ae2a6044ab8432e10
Reza Majidi <rezamajidizavie@gmail.com>
2018-10-22 00:15:11 +0330

    m

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,4 +3,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://rezamajidizavie:eldreza6620548@ds137643.mlab.com:37643/todo-api', {useNewUrlParser: true});
 
-module.exports={mongoose};
+module.exports = {mongoose};
+
+
+// mongoose.connect('mongodb://rezamajidizavie:eldreza6620548@ds137643.mlab.com:37643/todo-api', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use('/',express.static('public'));
 app.post('/todos', (req, res) => {
     let todo = new Todo({
         text: req.body.text
