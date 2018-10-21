99ea51fed962c803ca2415396dc9fd94af7dc908
Reza Majidi <rezamajidizavie@gmail.com>
2018-10-21 22:45:31 +0330

    m

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://rezamajidizavie:eldreza6620548>@ds137643.mlab.com:37643/todo-api' ,{useNewUrlParser: true});
+mongoose.connect('mongodb://rezamajidizavie:eldreza6620548@ds137643.mlab.com:37643/todo-api', {useNewUrlParser: true});
 
 module.exports={mongoose};
