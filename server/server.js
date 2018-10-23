f5834f8ba96c01960f737d950463a841f36ea4c2
Reza Majidi <rezamajidizavie@gmail.com>
2018-10-24 00:38:37 +0330

    m

--- server/server.js
+++ server/server.js
@@ -95,7 +95,7 @@ app.post('/users', (req, res) => {
         return user.generateAuthToken();
 
     }).then((token) => {
-        res.header('x-auth').send(user)
+        res.header('x-auth',token).send(user)
     }).catch((e) => {
         res.status(400).send(e)
     })
@@ -104,6 +104,18 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user)
 
+});
+
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth',token).send(user)
+        })
+    }).catch(()=>{
+        res.status(400).send();
+    })
 });
 
 app.listen(port, () => {
