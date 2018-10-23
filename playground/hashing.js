630cbb9ecf13ac95d13d97186b039917534b2407
Reza Majidi <rezamajidizavie@gmail.com>
2018-10-23 23:59:53 +0330

    m

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,16 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 10
-};
+let password = '123abc';
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash)
+//     })
+// });
 
+let hashedPassword = '$2a$10$5cnXmANuWzy7iEBfJECbzOzOLAH502750/Pn9Cn6WhohMsV2h6YNe';
 
-let decoded = jwt.verify(token,'123abc');
-console.log(decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res)
+});
